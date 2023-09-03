39d0418c4a6ad471d50d3c2d5b5697f8078f6c07
André Leli <andreleli.dev@gmail.com>
2023-09-02 23:49:18 -0300

    Primeiro projeto Flutter

---
+++ dioproject1/android/app/src/main/kotlin/com/companyname/packagename/dioproject1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.companyname.packagename.dioproject1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ parametros_app.md
@@ -0,0 +1,6 @@
+<!-- Parâmetros -->
+
+<!-- 1º) Criar uma aplicação: -->
+
+Flutter create -t app --org com.companyname.packagename -a kotlin -i swift nomedoapp
+
